41e75a3d6d6b0136083484915624705b15d483a8
Killian Gricourt <gricourt.killian@gmail.com>
2024-02-20 15:43:27 +0100

    Init with movable Hello wolrd!

--- app/src/main/java/com/example/suspension/MainActivity.java
+++ app/src/main/java/com/example/suspension/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.suspension;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+    private float offsetX, offsetY;
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        textView.setOnTouchListener((v, event) -> {
+            // Add your code here
+            return false;
+        });
     }
 }
